c35402db1e1be4e3693ea8e1e87900f46a06986f
SomaRe <shivatej82@gmail.com>
2021-05-24 16:42:01 +0530

    heroku url added

--- telegram-reddit-bot.py
+++ telegram-reddit-bot.py
@@ -161,7 +161,7 @@ def main() -> None:
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TOKEN)
-    updater.bot.setWebhook('https://yourherokuappname.herokuapp.com/' + TOKEN)
+    updater.bot.setWebhook(keys.heroku_url + TOKEN)
 
     # Block until you press Ctrl-C or the process receives SIGINT, SIGTERM or
     # SIGABRT. This should be used most of the time, since start_polling() is
