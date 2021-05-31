06db99fae3b4ad312c5567b2ddbc0e6df6f74073
SomaRe <shivatej82@gmail.com>
2021-05-31 10:21:26 +0530

    removed keys again

--- keys.py
+++
@@ -1,4 +0,0 @@
-TELE_TOKEN = "1859802191:AAGxJQp_-8n6NFvnXcta3LhwIOJBymwvub0"
-reddit_client_id="Ax-YuVWiXSBJDQ"
-reddit_client_secret="O7p8WDofP_zteexDk_dMMsXc7-xqwA"
-heroku_url = "https://tranquil-tor-75863.herokuapp.com/"
